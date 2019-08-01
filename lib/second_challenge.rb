f4fef9837d4e74290e5f765645e61c3ddda4a0d9
Katie Wokasch <katie.wokasch@gmail.com>
2019-08-01 21:47:46 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,7 +1,13 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriach => {
+      :patriarch => {
+        }
+      :matriarch => {
+        }
+      :hero => {
+        }
+      :hero_friends => {
         }
       }
       }, 
