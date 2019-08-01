069b8ca43f23dbfbef9859437e0ec0d4786e3d7e
Katie Wokasch <katie.wokasch@gmail.com>
2019-08-01 22:07:34 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,12 @@
 def first_challenge
-   epic_tragedy = {}
+   #sets a variable (epic_tragedy) equal to a hash containing keys of the family names and values of empty hashes
+   
+   epic_tragedy = {
+     :montague => {
+       
+     },
+     :capulet => {
+       
+     }
+   }
 end
