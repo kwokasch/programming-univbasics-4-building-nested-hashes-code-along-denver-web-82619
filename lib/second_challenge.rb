f463657a0bc0b40c8bff7ac92dc82b60faed3e98
Katie Wokasch <katie.wokasch@gmail.com>
2019-08-01 21:48:38 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -9,11 +9,19 @@ def second_challenge
         }
       :hero_friends => {
         }
-      }
       }, 
     :capulet => {
-      #your key/value pairs here!
-      }}
+      :patriarch => {
+        }
+      :matriarch => {
+        }
+      :heroine => {
+        }
+      :heroine_friends => {
+        }
+      }
+    
+  }
 
   
 end
